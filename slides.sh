49bf455193e68be614d383f2a6909db0b65a283c
Sameer <spuri4096@gmail.com>
2018-01-25 19:10:40 -0600

    Kill child processes properly and run forever

--- slides.sh
+++ slides.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
+while :
+do
 for i in *.{png,mp4}; do
+	echo $i
 	if [[ $i == *png ]]; then
-		mpv --loop $i &
+		mpv --loop "$i" &
 		sleep 10
-		kill %1
+		pkill -P $$
 		echo "BLAH"
 	elif [[ $i == *mp4 ]]; then
-		mpv $i
+		mpv "$i"
 		echo "BLEH"
 	fi
+done
 done
