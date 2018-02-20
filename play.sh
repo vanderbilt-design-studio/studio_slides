0c26f96e5933b1ab4bdf5b20566c8cb492dcaafe
Sameer <spuri4096@gmail.com>
2018-02-20 13:35:44 -0600

    Update play script

--- play.sh
+++ play.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while :
 do
-	ffmpeg -re -i 'udp://192.168.1.21:8080?listen' -pix_fmt bgra -f fbdev /dev/fb0
+	ffmpeg -loglevel panic -re -rtsp_flags listen -i 'rtsp://192.168.1.21:8080/live.sdp' -pix_fmt bgra -f fbdev /dev/fb0
 done
